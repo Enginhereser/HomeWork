b9510f981a76072eb114fda6b0b072161f87a008
engin <enginhereser@gmail.com>
2022-09-18 16:10:34 +0200

    HomeWord6 has been created.

--- src/HomeWork6/MainApp.java
+++ src/HomeWork6/MainApp.java
@@ -6,12 +6,12 @@ public class MainApp {
         Daire d1=new Daire();
 
         System.out.println("Daire...");
-        Daire daire = new Daire(-3.75);
+        Daire daire = new Daire(3.75);
         System.out.println("Yarıçap= " +daire.getYariCap());
         System.out.println("Alan= " +daire.getAlan());
         System.out.println("");
         System.out.println("Silindir...");
-        Silindir silindir = new Silindir(-5.55, -7.25);
+        Silindir silindir = new Silindir(5.55, 7.25);
         System.out.println("Yarıçap= " + silindir.getYariCap());
         System.out.println("Yükseklik= " + silindir.getYukseklik());
         System.out.println("Alan= " + silindir.getAlan());
